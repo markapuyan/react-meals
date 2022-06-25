5b1da51e87f53ed4ef14fda0489d7e3e445cbc1d
Mark <markapuyanph@gmail.com>
2022-06-25 16:12:33 +0700

    [Update] Add Context and Reducer

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,22 @@
+import { useContext } from 'react'
 import MealItemForm from './MealItemForm'
 import classes from './MealItem.module.css'
+import CartContext from '../../../store/cart-context'
 const MealItem = props => {
+    const cartCtx = useContext(CartContext)
     const price  = `$${props.price.toFixed(2)}`
+
+    const addToCartHandler = amount => {
+
+        console.log('here', amount)
+        console.log(cartCtx)
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
     return (
         <li className={classes.meal}>
             <div>
@@ -10,7 +25,7 @@ const MealItem = props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm/>
+                <MealItemForm onAddToCart={addToCartHandler}/>
             </div>
         </li>
     )
